0c68f550b1308780beec7d7bd2dbe0bac1d233d9
Maaato <lagos.matias25@gmail.com>
2021-04-01 00:31:49 -0300

    + PingCommand, + ReadyListener, + Client Updated

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,12 +1,36 @@
 import { AkairoClient, CommandHandler, ListenerHandler } from "discord-akairo";
 import { config } from "dotenv";
+import { join } from "path";
+
+config();
 
 export class WakmozClient extends AkairoClient {
+  private commandHandler: CommandHandler;
+  private listenerHandler: ListenerHandler;
+
   constructor() {
-    super();
+    super({ ownerID: process.env.OWNERID });
+    this.commandHandler = new CommandHandler(this, {
+      directory: join(__dirname, "..", "commands/"),
+      prefix: process.env.DISCORD_PREFIX,
+    });
+    this.listenerHandler = new ListenerHandler(this, {
+      directory: join(__dirname, "..", "listeners/"),
+    });
+  }
+
+  private _init() {
+    this.commandHandler.useListenerHandler(this.listenerHandler);
+    this.listenerHandler.setEmitters({
+      CommandHandler: this.commandHandler,
+      ListenerHandler: this.listenerHandler,
+    });
+    this.commandHandler.loadAll();
+    this.listenerHandler.loadAll();
   }
 
   public login(token: string) {
+    this._init();
     return super.login(token);
   }
 }
---
+++ src/listeners/Ready.ts
@@ -0,0 +1,15 @@
+import { Listener } from "discord-akairo";
+import { Logger } from "../utils/Logger";
+
+export default class ReadyListener extends Listener {
+  constructor() {
+    super("ready", {
+      emitter: "client",
+      event: "ready",
+    });
+  }
+
+  public exec() {
+    Logger.info(`${this.client.user!.tag} Up`, "ReadyListener");
+  }
+}
