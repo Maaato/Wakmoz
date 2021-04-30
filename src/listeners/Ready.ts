c59c53bd391868926bf5c856e6c594a0b9e32024
Maaato <lagos.matias25@gmail.com>
2021-04-29 21:57:40 -0400

    + Ready Listener

--- src/listeners/Ready.ts
+++ src/listeners/Ready.ts
@@ -1,27 +1,16 @@
 import { Listener } from "discord-akairo";
 
 import { Logger } from "../utils/Logger";
-import { AlmanaxService } from "../services/Almanax";
-import { DATETIME_NOW_MILI } from "../utils/Constants";
 
 export default class ReadyListener extends Listener {
-  private almanaxService;
   constructor() {
     super("ready", {
       emitter: "client",
       event: "ready",
     });
-    this.almanaxService = new AlmanaxService();
   }
 
   public exec() {
-    const { diff } = this.almanaxService.getRemainingTimeToNextBonus(DATETIME_NOW_MILI())
-    setTimeout(async () => {
-      Logger.info(`Discord ${this.client.user!.tag} Up`, "Listener");
-      const dayMillseconds = 1000 * 60 * 60 * 24;
-      setInterval(() => {
-        Logger.info(`Discord ${this.client.user!.tag} Up`, "Listener");
-      }, dayMillseconds);
-    }, diff);
+    Logger.info(`${this.client.user!.tag} Up`, "Listener");
   }
 }
